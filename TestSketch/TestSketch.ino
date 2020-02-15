d0a807c3ab46ff2a5a94c78c61c39408fa7c2f5d
thetechconspiracy <thetechconspiracy@outlook.com>
2020-02-15 00:38:48 -0500

    Everything should work.  I think one of the pots in my VP Pro is bad

--- TestSketch/TestSketch.ino
+++ TestSketch/TestSketch.ino
@@ -1,6 +1,6 @@
-int button[] = {0,1,2,3,4,5,6};
-int hat1[] = {7,8,9,10}; // Up, down, left, right
-int hat2[] = {11, A0, A4, A5};
+int button[] = {0, 1, 2, 3, 4, 5, 6};
+int hat1[] = {7, 8, 9, 10}; // Up, down, left, right
+int hat2[] = {11, A3, A4, A5};
 
 int yoke = A0;
 int push = A1;
@@ -9,11 +9,11 @@ void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
 
-  for(int i = 1; i <= 6; ++i){
+  for (int i = 1; i <= 6; ++i) {
     pinMode(button[i], INPUT_PULLUP);
   }
 
-  for(int i = 0; i < 4; ++i){
+  for (int i = 0; i < 4; ++i) {
     pinMode(hat1[i], INPUT_PULLUP);
     pinMode(hat2[i], INPUT_PULLUP);
   }
@@ -21,15 +21,15 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  for(int i = 1; i <= 6; ++i){
+  for (int i = 1; i <= 6; ++i) {
     Serial.print(digitalRead(button[i]));
   }
   Serial.print("\t");
-  for(int i = 0; i < 4; ++i){
+  for (int i = 0; i < 4; ++i) {
     Serial.print(digitalRead(hat1[i]));
   }
   Serial.print("\t");
-  for(int i = 0; i < 4; ++i){
+  for (int i = 0; i < 4; ++i) {
     Serial.print(digitalRead(hat2[i]));
   }
 
@@ -39,7 +39,7 @@ void loop() {
   Serial.print(analogRead(push));
   Serial.print("\t");
   Serial.print(analogRead(lever));
-  
+
   Serial.println();
-  
+
 }
